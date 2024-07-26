21a4590d2f3afbd4e809087696094886e568ed23
Danh Dinh <danhdinh98@gmail.com>
2024-07-26 11:42:15 +1000

    my first commit

---
+++ calculator-two.js
@@ -0,0 +1,22 @@
+// A simple calculator that takes two numbers and an operator (like +, -, *, /) as arguments, then outputs the result of that operation.
+var args = process.argv;
+var result = 0;
+
+num1 = parseInt(args[2]);
+operator = args[3];
+num2 = parseInt(args[4]);
+
+if (operator === '+') {
+    result = num1 + num2;
+}
+if (operator === '-') {
+    result = num1 - num2;
+}
+if (operator === '/') {
+    result = num1 / num2;
+}
+if (operator === 'x') {
+    result = num1 * num2;
+}
+
+console.log(`${num1} ${operator} ${num2} = ${result}`);
